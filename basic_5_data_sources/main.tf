c449b2a26bca50767066977f8438f17aa9623e2d
Christoph Caprano <Christoph.Caprano@valiton.com>
2020-12-14 17:10:22 +0100

    Add solution 5

---
+++ basic_5_data_sources/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "foo" {
+  ami             = data.aws_ami.amazon_ecs_optimized.id
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.allow_http.name]
+
+  tags = {
+    Name = "bar"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = aws_instance.foo.id
+  vpc      = true
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+
+  ingress {
+    description = "allow http traffic from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "amazon_ecs_optimized" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs*"]
+  }
+
+}
