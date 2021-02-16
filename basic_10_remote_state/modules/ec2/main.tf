e531029e7964c58d1d281f1bb188b0ee9cbb7ccf
Patrick Molz <patrick.molz@valiton.com>
2021-02-16 11:52:37 +0100

    Add solution to remote state task

---
+++ basic_10_remote_state/modules/ec2/main.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "foo" {
+  for_each        = var.names
+  ami             = data.aws_ami.amazon_ecs_optimized.id
+  instance_type   = var.instance_type
+  security_groups = var.security_groups
+  user_data       = <<EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;​
+echo "Hello from <?php echo gethostname(); ?>" > /var/www/html/index.php
+EOF
+
+  tags = {
+    Name = each.value
+  }
+}
+
+resource "aws_eip" "ip" {
+  for_each = aws_instance.foo
+
+  instance = each.value.id
+  vpc      = true
+}
+
+data "aws_ami" "amazon_ecs_optimized" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs*"]
+  }
+}
